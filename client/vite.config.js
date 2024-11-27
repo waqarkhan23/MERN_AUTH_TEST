080424e67ee889df18e432d26d951532e179ab9d
Waqar Khan <68925414+waqarkhan23@users.noreply.github.com>
2024-11-27 11:15:43 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,16 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    proxy: {
+      '/api': {
+        target: 'https://mern-auth-api-mu.vercel.app/', // Your API server URL
+        changeOrigin: true, // Handles virtual hosting in case of subdomains
+        secure: false, // If the target is HTTPS and you have self-signed certificates
+        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Remove `/api` prefix
+      },
+    },
+  },
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
